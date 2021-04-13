d8923dc7f8490739c2ad8fc81c4333f646309958
AbhiRam0099 <52668950+AbhiRam0099@users.noreply.github.com>
2021-04-13 15:48:12 -0500

    added routers

--- routes/vechile.js
+++ routes/vechile.js
@@ -7,3 +7,15 @@ router.get('/', vehicle_controlers.vechile_view_all_Page );
 module.exports = router;
 
 
+/* GET detail vechile page */
+router.get('/detail', vechile_controlers.vechile_view_one_Page);
+
+/* GET create vechile page */
+router.get('/create', vechile_controlers.vechile_create_Page);
+
+/* GET create update page */
+router.get('/update', vechile_controlers.vechile_update_Page);
+
+/* GET create vechile page */
+router.get('/delete', vechile_controlers.vechile_delete_Page);
+
